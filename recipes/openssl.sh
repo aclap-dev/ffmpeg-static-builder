ef24e0fd52c3c3863bc42092a8282cf0e714aae1
Paul Rouget <me@paulrouget.com>
2023-07-03 18:17:51 +0000

    tweaks

--- recipes/openssl.sh
+++ recipes/openssl.sh
@@ -1,8 +1,11 @@
 options=""
 
 function setup_cross {
-  if [[ $target_os == "windows" ]]; then
+  if [[ $target == "windows-x86_64" ]]; then
     options="mingw64 --cross-compile-prefix=$cross_toolchain_prefix-"
+  fi
+  if [[ $target == "windows-i686" ]]; then
+    options="mingw --cross-compile-prefix=$cross_toolchain_prefix-"
   fi
 }
 
